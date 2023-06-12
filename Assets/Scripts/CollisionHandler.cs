b0ce6e1e6747be68ce34cb3f1040a851c0b51b96
Marcin Knychała <12583215+mknycha@users.noreply.github.com>
2023-06-12 15:44:41 +0200

    Add tags to 'friendly' objects

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            default:
+                Debug.Log("Hit something");
+                break;
+        }
+    }
+}
